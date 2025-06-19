cde17a96a04dbd7b1bb8041ad6286c4f3dca3b34
Gadzhi Gadzhiev <47302253+Callisto7@users.noreply.github.com>
2025-06-19 21:04:53 +0300

    add carousel for cases

--- js/main.js
+++ js/main.js
@@ -27,26 +27,37 @@
 
     /* Testimonials Carousel 
     ========================================================*/
-    var owl = $("#testimonials");
+    var owl = $(".owl-carousel");
     owl.owlCarousel({
       loop: true,
       nav: false,
       dots: true,
-      center: true,
-      margin: 15,
-      slideSpeed: 1000,
+      center: false,
+      margin: 24,
+      slideSpeed: 8000,
       stopOnHover: true,
-      autoPlay: true,
+      autoplay: true,
       responsiveClass: true,
       responsiveRefreshRate: true,
       responsive: {
-        0: { items: 1 },
-        768: { items: 1 },
-        960: { items: 1 },
-        1200: { items: 1 },
-        1920: { items: 1 },
+        0: {
+          items: 1,
+        },
+        640: {
+          items: 1,
+        },
+        768: {
+          items: 2,
+        },
+        1024: {
+          items: 3,
+        },
+        1440: {
+          items: 3,
+        },
       },
     });
+
     const track = document.querySelector(".carousel-track");
     const items = document.querySelectorAll(".item");
 
